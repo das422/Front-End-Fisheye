771a3151402c244f2c5e712e7ccacc6ff8154c64
das422 <simonin974@gmail.com>
2024-02-17 09:19:48 +0400

    first

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,21 @@
+import NextAuth from 'next-auth';
+import EmailProvider from 'next-auth/providers/email';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+
+
+
+const prisma = new PrismaClient();
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM
+    })
+  ],
+});
+
+
+export { handler as GET, handler as POST }
