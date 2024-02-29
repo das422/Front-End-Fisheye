1d16c23873e14970d1f49a2d5246ed65d7b6bdde
das422 <simonin974@gmail.com>
2024-02-29 11:43:22 +0400

    get photographerdata to display

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -17,66 +17,68 @@ async function getDatabyId(id) {
 
 getDatabyId(Data);
 
-
 async function displayData() {
-    const { newPhotographer, mediaPhotographer } = await getDatabyId(Data);
+  const { newPhotographer, mediaPhotographer } = await getDatabyId(Data);
 
-
-    const article = document.createElement("article");
-    const h2 = document.createElement("h2");
-    const aTag = document.createElement("a");
-    const img = document.createElement("img");
-    const div = document.createElement("div");
-    const h3 = document.createElement("h3");
-    const p = document.createElement("p");
-    const span = document.createElement("span");
-    const ul = document.createElement("ul");
-    
-
-    aTag.textContent = newPhotographer.name;
-    img.setAttribute("src", `assets/photographers/${newPhotographer.portrait}`);
-    img.setAttribute("id", "photographers");
-    aTag.href = `/photographer.html?id=${newPhotographer.id}`;
-    h3.textContent = newPhotographer.city + "," + " " + newPhotographer.country;
-    p.textContent = newPhotographer.tagline;
-    span.textContent = newPhotographer.price + "€" + "/" + "jour";
-    
-    article.appendChild(aTag);
-    aTag.appendChild(img);
-    article.appendChild(div);
-    div.appendChild(h3);
-    div.appendChild(p);
-    div.appendChild(span);
-    article.appendChild(ul);
-    
-    mediaPhotographer.forEach((media) => {
-        const li = document.createElement("li");
-        const img = document.createElement("img");
-        const title = document.createElement("h2");
-        const likes = document.createElement("p");
-        const price = document.createElement("p");
-        const heart = document.createElement("i");
-    
-        img.setAttribute("src", `assets/${newPhotographer.name}/${media.image}`);
-        console.log(img);
-        img.setAttribute("id", "photographers");
-        title.textContent = media.title;
-        likes.textContent = media.likes;
-        price.textContent = media.price + "€";
-        heart.setAttribute("class", "fas fa-heart");
-    
-        ul.appendChild(li);
-        li.appendChild(img);
-        li.appendChild(title);
-        li.appendChild(likes);
-        li.appendChild(price);
-        li.appendChild(heart);
-    });
-    
-    return article;
-}
+  
+  const article = document.createElement("article");
  
+
+  const h2 = document.createElement("h2");
+  const aTag = document.createElement("a");
+  const img = document.createElement("img");
+  const div = document.createElement("div");
+  const h3 = document.createElement("h3");
+  const p = document.createElement("p");
+  const span = document.createElement("span");
+  const ul = document.createElement("ul");
+
+  aTag.textContent = newPhotographer.name;
+  img.setAttribute("src", `assets/photographers/${newPhotographer.portrait}`);
+  img.setAttribute("id", "photographers");
+  aTag.href = `/photographer.html?id=${newPhotographer.id}`;
+  h3.textContent = newPhotographer.city + "," + " " + newPhotographer.country;
+  p.textContent = newPhotographer.tagline;
+  span.textContent = newPhotographer.price + "€" + "/" + "jour";
+
+  article.appendChild(aTag);
+  aTag.appendChild(img);
+  article.appendChild(div);
+  div.appendChild(h3);
+  div.appendChild(p);
+  div.appendChild(span);
+  article.appendChild(ul);
+
+  mediaPhotographer.forEach((media) => {
+    const li = document.createElement("li");
+    const img = document.createElement("img");
+    const title = document.createElement("h2");
+    const likes = document.createElement("p");
+    const price = document.createElement("p");
+    const heart = document.createElement("i");
+
+    img.setAttribute("src", `assets/${newPhotographer.name}/${media.image}`);
+    console.log(img);
+    img.setAttribute("id", "photographers");
+    title.textContent = media.title;
+    likes.textContent = media.likes;
+    price.textContent = media.price + "€";
+    heart.setAttribute("class", "fas fa-heart");
+
+    ul.appendChild(li);
+    console.log(ul);
+    li.appendChild(img);
+    li.appendChild(title);
+    li.appendChild(likes);
+    li.appendChild(price);
+    li.appendChild(heart);
+  });
+  console.log(article);
+  return article;
+}
+
 displayData().then((article) => {
-    const photographerSection = document.querySelector(".photographer_section");
-    photographerSection.appendChild(article);
+  const main = document.querySelector("#main");
+  console.log(main);
+  main.appendChild(article);
 });
