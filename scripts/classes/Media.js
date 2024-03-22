5b07dcb8603a10d833a49f8b621769446e5ee45f
das422 <simonin974@gmail.com>
2024-03-22 17:01:26 +0400

    Update script tags to use ES modules

---
+++ scripts/classes/Media.js
@@ -0,0 +1,26 @@
+export default class Media{
+  constructor(data) {
+    this.id = data.id;
+    this.photographerId = data.photographerId;
+    this.title = data.title;
+    this.alt = data.alt;
+    this.tags = data.tags;
+    this.likes = data.likes;
+    this.date = data.date;
+    this.price = data.price;
+  }
+}
+
+export class Image extends Media {
+  constructor(data) {
+    super(data);
+    this.image = data.image;
+  }
+};
+
+export class Video extends Media {
+  constructor(data) {
+    super(data);
+    this.video = data.video;
+  }
+};
