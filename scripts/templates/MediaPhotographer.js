d30488d628e4e387dab340771177076d01347be3
das422 <simonin974@gmail.com>
2024-03-25 08:00:12 +0400

    Update photographer.css and photographer.html

--- scripts/templates/MediaPhotographer.js
+++ scripts/templates/MediaPhotographer.js
@@ -6,18 +6,20 @@ export default class MediaPhotographer {
 
   createMediaPhotographer() {
     const photographerContent = document.querySelector(".main_content-medias");
-    const MediaCard = `<section class="galeery">
+    const MediaCard = `<section class="gallery">
     ${this.medias.map(media => {
       const mediaType = media.image
         ? `<img class="gallery_preview" src="./assets/medias/${this.photographer.name}/${media.image}" alt="${media.alt}">`
-        : `<video class="gallery_thumbnail" aria-label="${media.alt}">
+        : `<video class="gallery_preview" aria-label="${media.alt}">
         <source src="./assets/medias/${this.photographer.name}/${media.video}" type="video/mp4">
     </video>`;
       return `<div class="gallery_card">
-      <a href="#" data-media=${media.id} role="link" aria-label="Display media full screen">
+      <div class="gallery_card__media">
+            <a href="#" data-media=${media.id} role="link" aria-label="Display media full screen">
       <figure>${mediaType}</figure>
       </a>
-      <figcaption>
+      </div>
+      <figcaption class="legend">
       <h2>${media.title}</h2>
           <div role="group" aria-label="Like button and number of likes">
               <span class="nbLike">${media.likes}</span> 
